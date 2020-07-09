2acbc8d1e996996fbe1c2207095dfe390e8d5720
Matheus Quost <matheusquost@gmail.com>
2020-07-09 19:22:30 -0300

    Atualizando com a atividade entregue na AER2 de Dev. web. I

--- js/script.js
+++ js/script.js
@@ -68,4 +68,21 @@ $(document).ready(function() {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
+
+});
+
+$(function() {
+    var str = '#len'; //increment by 1 up to 1-nelemnts
+    $(document).ready(function() {
+        var i, stop;
+        i = 1;
+        stop = 4; //num elements
+        setInterval(function() {
+            if (i > stop) {
+                return;
+            }
+            $('#len' + (i++)).toggleClass('bounce');
+        }, 500)
+    });
 });
